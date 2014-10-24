5d633df02c0f7337a7db94721f9eae8d41c7090b
Natasha Jones <tasha.k.jones@gmail.com>
2014-10-24 11:53:13 -0400

    add FlickRaw url helper

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,19 +1,13 @@
 require 'flickraw'
 
 class PhotosController < ApplicationController
-
   def index
     FlickRaw.api_key = ENV['FlickRaw_api_key']
     FlickRaw.shared_secret = ENV['FlickRaw_shared_secret']
 
-    @photosets = flickr.photosets.getList(user_id: '67921947@N02').each do |set|
-      id_array = []
-      id_array.push(set['id'])
-      id_array.each { |i| puts '*' * 50 + i } #just here to see what's go'n on.
-      @albums = id_array.each do |i|
-        flickr.photosets.getPhotos(photoset_id: i) do |album|
-          puts album
-        end
+    @photosets = flickr.photosets.getList(user_id: '67921947@N02').map do |set|
+      flickr.photosets.getPhotos(photoset_id: set['id']).photo.map do |photo|
+        FlickRaw.url_b(photo)
       end
     end
   end
@@ -22,3 +16,6 @@ end
 # for each photoset I want to use the id and title
 # inject the id into a search and use the title as a reference
 # flickr.photo.search(user_id: '67921947@N02', '#{set}')
+
+# get photo sets
+# loop through photosets and return links
